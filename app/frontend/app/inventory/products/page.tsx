57d001caf46dd163b4d2c17fbaa42f6f32189fb8
aspiratio-eng <aspiratio-eng@yahoo.co.jp>
2025-03-20 10:25:21 +0900

    ダミーデータを読み込んで商品一覧を表示するよう変更

--- app/frontend/app/inventory/products/page.tsx
+++ app/frontend/app/inventory/products/page.tsx
@@ -1,4 +1,23 @@
+"use client"
+
+import { useState, useEffect } from "react"
+import productsData from "./sample/dummy_products.json"
+import Link from "next/link"
+
+type ProductData = {
+  id: number
+  name: string
+  price: number
+  description: string
+}
+
 export default function Page() {
+  const [data, setData] = useState<Array<ProductData>>([])
+
+  useEffect(() => {
+    setData(productsData)
+  }, [])
+
   return (
     <>
       <h2>商品一覧</h2>
@@ -14,39 +33,20 @@ export default function Page() {
           </tr>
         </thead>
         <tbody>
-          <tr>
-            <td>1</td>
-            <td>コットン100%バックリボンティアードワンピース（黒）</td>
-            <td>6900</td>
-            <td>
-              大人の愛らしさを引き立てる、ナチュラルな風合い。リラックス×トレンドを楽しめる、上品なティアードワンピース。
-            </td>
-            <td>
-              <button>更新・削除</button>
-            </td>
-          </tr>
-          <tr>
-            <td>2</td>
-            <td>ライトストレッチカットソー（ネイビー）</td>
-            <td>2980</td>
-            <td>
-              しなやかな肌触りが心地よい、程よいフィット感のカットソー。ビジネスカジュアルにも普段使いにも使える、ベーシックなデザイン。
-            </td>
-            <td>
-              <button>更新・削除</button>
-            </td>
-          </tr>
-          <tr>
-            <td>3</td>
-            <td>ベルト付きデニムパンツ（ブルー）</td>
-            <td>5980</td>
-            <td>
-              定番のデニムパンツに、フェミニンなベルトをプラスしたスタイリッシュなアイテム。カジュアルにも綺麗めにも合わせやすい。
-            </td>
-            <td>
-              <button>更新・削除</button>
-            </td>
-          </tr>
+          {data.map((data: any) => (
+            <tr key={data.id}>
+              <td>{data.id}</td>
+              <td>{data.name}</td>
+              <td>{data.price}</td>
+              <td>{data.description}</td>
+              <td>
+                <Link href={`/inventory/products/${data.id}`}>在庫処理</Link>
+              </td>
+              <td>
+                <button>更新・削除</button>
+              </td>
+            </tr>
+          ))}
         </tbody>
       </table>
     </>
