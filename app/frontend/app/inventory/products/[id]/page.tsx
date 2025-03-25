bc64d9f02b6b324c51035ab51c3452d5235e6762
aspiratio-eng <aspiratio-eng@yahoo.co.jp>
2025-03-26 06:11:40 +0900

    商品在庫画面にバリデーションを追加

--- app/frontend/app/inventory/products/[id]/page.tsx
+++ app/frontend/app/inventory/products/[id]/page.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import { useForm } from "react-hook-form"
 import { use, useState, useEffect } from "react"
 import productsData from "../sample/dummy_products.json"
 import inventoriesData from "../sample/dummy_inventories.json"
@@ -9,6 +10,11 @@ type ProductData = {
   name: string
   price: number
   description: string
+}
+
+type FormData = {
+  id: number
+  quantity: number
 }
 
 type InventoryData = {
@@ -24,6 +30,12 @@ type InventoryData = {
 export default function Page({ params }: { params: Promise<{ id: number }> }) {
   const { id } = use(params)
 
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm()
+
   // 読み込みデータを保持
   const [product, setProduct] = useState<ProductData>({
     id: 0,
@@ -32,7 +44,10 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
     description: "",
   })
 
+  // 読み込みデータを保持
   const [data, setData] = useState<Array<InventoryData>>([])
+  // submit時のactionを分岐させる
+  const [action, setAction] = useState<string>("")
 
   useEffect(() => {
     const selectedProduct: ProductData = productsData.find(
@@ -42,21 +57,60 @@ export default function Page({ params }: { params: Promise<{ id: number }> }) {
     setData(inventoriesData)
   }, [params])
 
+  const onSubmit = (event: any): void => {
+    const data: FormData = {
+      id: Number(id),
+      quantity: Number(event.quantity),
+    }
+
+    // actionによってHTTPメソッドと使用するパラメータを切り替える
+    if (action === "purchase") {
+      handlePurchase(data)
+    } else if (action === "sell") {
+      if (data.id === null) {
+        return
+      }
+      handleSell(data)
+    }
+  }
+
+  // 仕入れ・卸し処理
+  const handlePurchase = (data: FormData) => {
+    console.log(`仕入れ ID: ${data.id}, 数量: ${data.quantity}`)
+  }
+
+  const handleSell = (data: FormData) => {
+    console.log(`卸し ID: ${data.id}, 数量: ${data.quantity}`)
+  }
+
   return (
     <>
       <h2>商品在庫</h2>
       <h3>在庫処理</h3>
-      <form>
+      <form onSubmit={handleSubmit(onSubmit)}>
         <div>
           <label>商品名</label>
           <span>{product.name}</span>
         </div>
         <div>
           <label>数量：</label>
-          <input type="text" />
+          <input
+            type="number"
+            id="quantity"
+            {...register("quantity", {
+              required: "必須入力です",
+              min: 1,
+              max: 99999999,
+            })}
+          />
+          {errors.quantity && <div>1から99999999の数値を入力してください</div>}
         </div>
-        <button>商品を仕入れる</button>
-        <button>商品を卸す</button>
+        <button type="submit" onClick={() => setAction("purchase")}>
+          商品を仕入れる
+        </button>
+        <button type="submit" onClick={() => setAction("sell")}>
+          商品を卸す
+        </button>
       </form>
       <h3>在庫履歴</h3>
       <table>
