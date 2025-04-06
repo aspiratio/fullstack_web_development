9d3a13ef781f2976925e247907a253de34156c2c
aspiratio-eng <aspiratio-eng@yahoo.co.jp>
2025-04-07 06:32:12 +0900

    参照系のAPIを作成

--- app/backend/config/urls.py
+++ app/backend/config/urls.py
@@ -4,5 +4,6 @@ from django.urls import include, path
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/hello/", include("api.hello.urls")),
-    path("api/hello_db/", include("api.hello_db.urls"))
+    path("api/hello_db/", include("api.hello_db.urls")),
+    path("api/inventory/", include("api.inventory.urls"))
 ]
